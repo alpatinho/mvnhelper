af38ec5b9087bf856efbaffac21b8fbd25168b1d
Doug <doug.paz@hotmail.com>
2018-03-24 22:17:10 -0300

    "Elementos da cena setados"

--- src/FxGUI/Main.java
+++ src/FxGUI/Main.java
@@ -6,18 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
-        primaryStage.setTitle("MVN Helper V_0.1");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+public class Main extends Application{
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        //Bota o titulo na janela
+        primaryStage.setTitle("MVN Helper V_0.1");
+
+        //carrega a cena (conteudo da pagina) no root
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+
+        //carrega a cena no stage primario
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
 }
